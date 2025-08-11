ef797e47e35f84398d4ffc2d370a0aacdea5bd7b
Komal7834 <komalsu1910@gmail.com>
2025-08-11 09:57:26 +0530

    My portfolio website commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,29 @@
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils"
+
+export const About = () => {
+    return (
+        <section className={styles.container} id="about" >
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content} >
+                <img src={getImageUrl("about/aboutImage.png")} alt="Me sitting with a laptop"
+                className={styles.aboutImage}  />
+                <ul className={styles.aboutItems}  >
+                    <li className={styles.aboutItem}  ><img src={getImageUrl("about/cursorIcon.png") } alt="cursor icon" />
+                    <div className={styles.aboutItemText} >
+                        <h3> Frontend Developer</h3>
+                        <p>
+                            I'm a Frontend developer with experience in building responsive and optimized sites.
+                            </p></div> </li>
+                            <li className={styles.aboutItem} ><img src={getImageUrl("about/serverIcon.png") } alt="server icon" />
+                    <div className={styles.aboutItemText}>
+                        <h3> Backend Developer</h3>
+                        <p>
+                            I have experience in developing fast and optimized backend sysytems and APIs
+                            </p></div> </li>
+                </ul>
+            </div>
+        </section>
+    )
+}
